be81d8b0ccf4715b1136ebf386a1787f5a6b1ba4
r616on <r616on@gmail.com>
2021-11-14 11:00:01 +0300

    two task_done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import Stars from "./components/Stars/Stars";
+import Listing from "./components/Listing/Listing";
+import data from "./etsy.json";
 
 function App() {
   return (
     <div className="App">
       <Stars count={4} />
+      <Listing items={data} />
     </div>
   );
 }
---
+++ src/components/Listing/Listing.js
@@ -0,0 +1,78 @@
+import "./desktop.scss";
+import React from "react";
+
+function titleFun(title) {
+  if (title) {
+    let str = "";
+    if (title.length > 50) {
+      str = title.substring(0, 50) + "...";
+    } else {
+      str = title;
+    }
+    return str;
+  }
+  return false;
+}
+function currencyPrice(price, currency) {
+  if (price && currency) {
+    if (currency === "USD") {
+      return `$${price}`;
+    } else if (currency === "EUR") {
+      return `€${price}`;
+    } else {
+      return `${price} ${currency}`;
+    }
+  }
+}
+
+function levelClass(quantity) {
+  if (quantity < 11) {
+    return "level-low";
+  } else if (quantity > 10 && quantity < 21) {
+    return "level-medium";
+  } else if (quantity > 20) {
+    return "level-high";
+  }
+}
+
+function Listing({ items }) {
+  return (
+    <div className="item-list">
+      {items.map((item) => {
+        const {
+          listing_id,
+          url,
+          MainImage,
+          title,
+          currency_code,
+          price,
+          quantity,
+        } = item;
+
+        return (
+          <div key={listing_id} className="item">
+            <div className="item-image">
+              <a href={url}>
+                <img
+                  src={MainImage ? MainImage.url_570xN : ""}
+                  alt={titleFun(title) ? titleFun(title) : ""}
+                />
+              </a>
+            </div>
+            <div className="item-details">
+              <p className="item-title">{titleFun(title)}</p>
+              <p className="item-price">
+                {currencyPrice(price, currency_code)}
+              </p>
+              <p className={`item-quantity ${levelClass(quantity)}`}>
+                {quantity}
+              </p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Listing;
